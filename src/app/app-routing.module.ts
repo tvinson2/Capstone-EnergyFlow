1c6558c517a2db5140fc87f1f196fb9c2869fd9b
tvinson2 <thomasv330@gmail.com>
2020-09-10 17:52:33 -0400

    routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-
-
+import { CrownCComponent } from './crown/crown-c/crown-c.component';
+import { HeartCComponent } from './heart/heart-c/heart-c.component';
+import { InfoCComponent } from './info/info-c/info-c.component';
+import { MainCComponent } from './main/main-c/main-c.component';
+import { RootCComponent } from './root/root-c/root-c.component';
+import { SacralCComponent } from './sacral/sacral-c/sacral-c.component';
+import { SolarCComponent } from './solar/solar-c/solar-c.component';
+import { ThirdeyeCComponent } from './thirdeye/thirdeye-c/thirdeye-c.component';
+import { ThroatCComponent } from './throat/throat-c/throat-c.component';
+import { WelcomeCComponent } from './welcome/welcome-c/welcome-c.component';
 
 const routes: Routes = [
-
- 
-
-  
-  { path: 'crown',  loadChildren: () => import('./crown/crown.module').then(m => m.CrownModule) },
-  { path: 'heart',  loadChildren: () => import('./heart/heart.module').then(m => m.HeartModule) },
-  { path: 'info',  loadChildren: () => import('./info/info.module').then(m => m.InfoModule) },
-  { path: 'root',  loadChildren: () => import('./root/root.module').then(m => m.RootModule) },
-  { path: 'throat',  loadChildren: () => import('./throat/throat.module').then(m => m.ThroatModule) },
-  { path: 'thirdeye',  loadChildren: () => import('./thirdeye/thirdeye.module').then(m => m.ThirdeyeModule) },
-  { path: 'solar',  loadChildren: () => import('./solar/solar.module').then(m => m.SolarModule) },
-  { path: 'sacral',  loadChildren: () => import('./sacral/sacral.module').then(m => m.SacralModule) },
-
-  { path: '', redirectTo: '', pathMatch: 'full' }
-
-
+{path:'',component:WelcomeCComponent},
+{path:'app-crown-c',component:CrownCComponent},{path:'app-heart-c',component:HeartCComponent},
+{path:'app-info-c',component:InfoCComponent},
+{path:'app-main-c',component:MainCComponent},
+{path:'app-root-c',component:RootCComponent},
+{path:'app-sacral-c',component:SacralCComponent},
+{path:'app-solar-c',component:SolarCComponent},
+{path:'app-thirdeye-c',component:ThirdeyeCComponent},
+{path:'app-throat-c',component:ThroatCComponent},
+{path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }14014111
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatButtonModule } from '@angular/material/button';  /* slide over menu */
 import { MatIconModule } from '@angular/material/icon';  /* slide over menu */
 import { MatSidenavModule } from '@angular/material/sidenav';  /* slide over menu */
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-//import { MainCComponent } from './main-c/main-c.component'; 
+
+
+///////////////////
+import { CrownCComponent } from './crown/crown-c/crown-c.component';
+import { HeartCComponent } from './heart/heart-c/heart-c.component';
+import { InfoCComponent } from './info/info-c/info-c.component';
+import { MainCComponent } from './main/main-c/main-c.component';
+import { RootCComponent } from './root/root-c/root-c.component';
+import { SacralCComponent } from './sacral/sacral-c/sacral-c.component';
+import { SolarCComponent } from './solar/solar-c/solar-c.component';
+import { ThirdeyeCComponent } from './thirdeye/thirdeye-c/thirdeye-c.component';
+import { ThroatCComponent } from './throat/throat-c/throat-c.component';
+import { WelcomeCComponent } from './welcome/welcome-c/welcome-c.component';
+//////////////////
 
 
 @NgModule({
   declarations: [
-    AppComponent
-   // MainCComponent
+    AppComponent,
+    CrownCComponent,
+    HeartCComponent,
+    InfoCComponent,
+    MainCComponent,
+    RootCComponent,
+    SacralCComponent,
+    SolarCComponent,
+    ThirdeyeCComponent,
+    ThroatCComponent,
+    WelcomeCComponent,
     
   ],
   imports: [
@@ -27,8 +46,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule, /* slide over menu button */
     MatIconModule,  /* slide over menu icon */
     MatSidenavModule,  /* slide over menu sidenave */
-    MatToolbarModule
-    
+    MatToolbarModule,
+    ////////////////
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
